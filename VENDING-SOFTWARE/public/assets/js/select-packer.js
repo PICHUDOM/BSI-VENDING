c7f90c1a5322e76cd34019fafb992de5cd397dc0
Lin <lin997808gmail.com>
2023-12-20 10:05:14 +0700

    Active ,sidbar, form create

--- VENDING-SOFTWARE/public/assets/js/select-packer.js
+++ VENDING-SOFTWARE/public/assets/js/select-packer.js
@@ -12,3 +12,13 @@ $(document).ready(function () {
     });
 });
 
+$(function(){
+    var current = location.pathname;
+    $('#sidebar ul li a').each(function(){
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if($this.attr('href').indexOf(current) !== -1){
+            $this.addClass('active');
+        }
+    })
+})
